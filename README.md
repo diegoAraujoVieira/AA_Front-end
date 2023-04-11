486fb61c2c87232bd68c07d083dc20de8ede8eba
Diego Araujo <105077393+diegoAraujoVieira@users.noreply.github.com>
2023-04-11 11:54:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AA_Front-end
+ Trabalho da disciplina de Construção de Front-end. Foi desenvolvido o site de uma cafeteria utilizando HTML, CSS e Javascript.
---
+++ js/script.js
@@ -0,0 +1,8 @@
+//código da API ScrollReveal que revela elementos conforme a rolagem do site
+window.sr = ScrollReveal({ reset: false });
+
+sr.reveal('#espaço', {duration: 2000 });
+
+sr.reveal('#cardapio', {duration: 2000 });
+
+sr.reveal('#endereco', {duration: 2000 });
